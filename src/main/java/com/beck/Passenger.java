202d458877eb8dd22b217dc004fe6a96682647dd
Bekbolot E <bekoxna@gmail.com>
2025-08-05 21:16:26 +0600

    Added new method to the Passenger class

--- src/main/java/com/beck/Passenger.java
+++ src/main/java/com/beck/Passenger.java
@@ -8,6 +8,7 @@ public class Passenger {
     private String identifier;
     private String name;
     private String countryCode;
+    private Flight flight;
 
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)){
@@ -16,6 +17,29 @@ public class Passenger {
         this.identifier = identifier;
         this.name = name;
         this.countryCode = countryCode;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void joinFlight(Flight flight) {
+        Flight oldFlight = this.flight;
+        if (oldFlight != null) {
+            if (!oldFlight.removePassenger(this)) {
+                throw new RuntimeException("Failed to remove passenger from the flight");
+            }
+        }
+        setFlight(flight);
+        if (flight != null) {
+            if (!flight.addPassenger(this)) {
+                throw new RuntimeException("Failed to add passenger to the flight");
+            }
+        }
     }
 
     public String getIdentifier() {
